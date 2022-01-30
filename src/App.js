8d28d1e5c51f2a0c4ae27d3c88aa689e3c5df1fa
18059819d <18059819d@connect.polyu.hk>
2022-01-31 00:28:19 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <main>
       <Header />
       <About />
+      <Projects />
+      <Skills />
     </main>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
-
-function Header() {
-    return(
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='py-4'>
-        <Container>
-            <Navbar.Brand href="#home" >Aaron Yuen</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="#about">About</Nav.Link>
-                    <Nav.Link href="#project">Project</Nav.Link>
-                    <Nav.Link href="#skills">Skills</Nav.Link>
-                </Nav>
-                {/* <Button variant="outline-success">Contact Me</Button> */}
-            </Navbar.Collapse>
+import React from 'react'
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+const Header = () => {
+    return (
+        <Navbar bg="dark" expand="lg" sticky="top" variant="dark" className="py-4" collapseOnSelect>
+            <Container fluid="md">
+                <Navbar.Brand href="/portfolio" style={{fontSize:"26px", borderRight:"2px solid grey", padding:"0 10px"}}>Aaron Yuen</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{padding: "0 10px"}}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#skills">Skills</Nav.Link>
+                    </Nav>
+                    <Button 
+                        href="#contact" 
+                        className="d-flex" 
+                        variant="outline-success" 
+                        size="md"
+                        style={{margin: "0 10px", maxWidth: "25vw", justifyContent: "center"}}
+                    >
+                        Contact Me
+                    </Button>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
-}
+};
 
 export default Header;
